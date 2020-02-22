519c916189f3f4d40f06e302da7f3655c1578ba9
MateuszTk <41924838+MateuszTk@users.noreply.github.com>
2020-02-22 09:29:41 +0100

    Add files via upload

---
+++ main/kostkownik/kamerka.py
@@ -0,0 +1,62 @@
+import imutils
+from imutils.video import VideoStream
+import numpy as np
+import cv2
+
+import time
+
+#greenLower = (165, 86, 174)
+#greenUpper = (203, 208, 255)
+
+greenLower = (0, 87, 193)
+greenUpper = (8, 233, 255)
+
+vs = VideoStream(src=0).start()
+
+time.sleep(2.0)
+
+while True:
+	# grab the current frame
+	frame = vs.read()
+	#frame = frame[1]
+	
+	if frame is None:
+		break
+	frame = imutils.resize(frame, width=600)
+	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+	mask = cv2.inRange(hsv, greenLower, greenUpper)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+	
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	center = None
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+		# only proceed if the radius meets a minimum size
+		if radius > 10:
+			# draw the circle and centroid on the frame,
+			# then update the list of tracked points
+			cv2.circle(frame, (int(x), int(y)), int(radius),
+				(0, 255, 255), 2)
+			cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+	
+	
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+		break
+		
+vs.stop()
+cv2.destroyAllWindows()
